bf745b3abec51f82c0c3a7e4d30b830d1a4fbf66
Christian <chrisnebech@hotmail.com>
2022-08-30 18:47:32 -0300

    começo do requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input type="text" data-testid="login-name-input" />
+        <button type="submit" onClick={ createUser }>Entrar</button>
+      </div>
     );
   }
 }
