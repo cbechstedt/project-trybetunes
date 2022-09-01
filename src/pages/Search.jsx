ed85f213de3448beaf649a85b30875b684f31ce3
Christian <chrisnebech@hotmail.com>
2022-09-01 17:58:30 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,40 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearch: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      inputSearch: event.target.value,
+    });
+  };
+
   render() {
+    const { inputSearch } = this.state;
+    const minSearchLength = 2;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          placeholder="Nome do Artista"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          // onClick={ this.handleClick }
+          disabled={ inputSearch.length < minSearchLength }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
