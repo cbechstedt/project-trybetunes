65ff28997f57c8a41c320b79cbc14f4c3338e6fb
Christian <chrisnebech@hotmail.com>
2022-08-31 21:09:17 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputLogin: '',
+      loading: false,
+      redirecting: false,
+    };
+  }
+
+  handleClick = async () => {
+    this.setState({
+      loading: true,
+    });
+    const { inputLogin } = this.state;
+    await createUser({ name: inputLogin });
+    this.setState({
+      loading: false,
+      redirecting: true,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      inputLogin: event.target.value,
+    });
+  };
+
   render() {
+    const { inputLogin, loading, redirecting } = this.state;
+    const minLoginLength = 3;
     return (
       <div data-testid="page-login">
-        <input type="text" data-testid="login-name-input" />
-        <button type="submit" onClick={ createUser }>Entrar</button>
+        <input
+          type="text"
+          data-testid="login-name-input"
+          placeholder="Nome"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="submit"
+          onClick={ this.handleClick }
+          disabled={ inputLogin.length < minLoginLength }
+        >
+          Entrar
+        </button>
+        {loading && <Loading /> }
+        {redirecting && <Redirect to="/search" />}
       </div>
     );
   }
